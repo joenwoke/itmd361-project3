693f38ae37aa9d6b87033cfc96f0f29de09f1cd5
joenwoke <jnwoke@hawk.iit.edu>
2023-11-30 00:42:12 -0600

    changed link to script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function initMap() {
   var location = {lat: 41.831300, lng: 87.627270};
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 4, 
+    zoom: 12, 
     center: location
   });
   var marker = new google.maps.Marker({
