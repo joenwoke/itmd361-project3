b1f6ce754af8b65889436f8fdd1558f0473f0de9
joenwoke <jnwoke@hawk.iit.edu>
2023-11-30 22:21:30 -0600

    Changed coordinates for map

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function initMap() {
-  var location = {lat: 41.831300, lng: 87.627270};
+  var location = {lat: 41.8349609375, lng: -87.62698364257812};
   var map = new google.maps.Map(document.getElementById("map"), {
     zoom: 12, 
     center: location
