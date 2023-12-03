fb96856e8e109a915d33ed60dab9b7da0111ca33
joenwoke <jnwoke@hawk.iit.edu>
2023-12-03 12:04:12 -0600

    Updated CSS and zoom for javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,16 @@
 function initMap() {
   var location = {lat: 41.8349609375, lng: -87.62698364257812};
   var map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 12, 
+    zoom: 15, 
     center: location
   });
+  
   var marker = new google.maps.Marker({
     position: location,
     map: map
   });
+  
+  var trafficLayer = new google.maps.TrafficLayer();
+  trafficLayer.setMap(map);
 }
+window.initMap = initMap;
